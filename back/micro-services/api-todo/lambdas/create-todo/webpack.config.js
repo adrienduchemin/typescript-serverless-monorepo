9d42c41ec2f6a9d10830bcbc45d92af6b8cd7c8c
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-11-28 04:20:34 +0000

    fix few  errors

--- back/micro-services/api-todo/lambdas/create-todo/webpack.config.js
+++ back/micro-services/api-todo/lambdas/create-todo/webpack.config.js
@@ -1,15 +1,19 @@
-const path = require("path");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-  mode: "production",
-  target: "node",
-  entry: path.resolve(__dirname, "lib/index.js"),
+  mode: 'production',
+  target: 'node',
+  entry: path.resolve(__dirname, 'lib/index.js'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
+    plugins: [new TsconfigPathsPlugin()],
   },
-  externals: [nodeExternals()], //+ external aws-sdk et aws-lambda ...
-};
+  // externals: [nodeExternals()], //+ external aws-sdk et aws-lambda ...
+}
