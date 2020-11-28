ead0cd2c57f39a0af94eea77b220d82d8a23738d
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-11-28 16:43:38 +0000

    clean envs

--- back/micro-services/api-todo/lambdas/create-todo/src/index.ts
+++ back/micro-services/api-todo/lambdas/create-todo/src/index.ts
@@ -5,7 +5,6 @@ import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
 import { initConfig } from './init'
 import {
   handle,
-  HttpBadRequestError,
   HttpError,
   HttpInternalServerError,
   IAPIGatewayErrorPayload,
@@ -32,16 +31,11 @@ export const handler: APIGatewayProxyHandlerV2<Todo> = async (
     const response = await handle(event, config)
     return response
   } catch (err) {
-    console.log(err instanceof HttpError)
-    console.log(err instanceof HttpBadRequestError)
-    console.log(err instanceof HttpInternalServerError)
-
-    // maybe should spread for shallow copy
     let httpError: HttpError = err
     if (!(err instanceof HttpError)) {
       httpError = new HttpInternalServerError({
         error: 'Oops, Something Went Wrong',
-        trace: process.env.NODE_ENV !== 'production' ? err : undefined,
+        trace: config.env.environment !== 'production' ? err : undefined,
       })
     }
     const { statusCode, message, details } = httpError
