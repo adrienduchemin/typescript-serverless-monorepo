ba46e4e488b20ce4b3e5c28cc724c8bef751d769
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-11-29 19:02:37 +0000

    improve fanout

--- back/stacks/todos-api/lambdas/fanout/src/main.ts
+++ back/stacks/todos-api/lambdas/fanout/src/main.ts
@@ -1,71 +1,59 @@
+import { ITodo } from '@shared-types'
 import { DynamoDBStreamEvent } from 'aws-lambda'
-import {
-  PutEventsRequest,
-  PutEventsRequestEntryList,
-} from 'aws-sdk/clients/cloudwatchevents'
+import { PutEventsRequest } from 'aws-sdk/clients/cloudwatchevents'
+import { Converter } from 'aws-sdk/clients/dynamodb'
 
 import { IConfig } from './init'
-
-interface EventBridgeEntry {
-  eventType: string
-  todo: unknown
-}
 
 export const handle = async (
   event: DynamoDBStreamEvent,
   { eventbridge }: IConfig
 ): Promise<void> => {
-  const entries: EventBridgeEntry[] = []
-
-  event.Records.forEach((record) => {
+  // maybe should for of with async and send them one by one to eventbridge
+  // so each error can be sent individually instead of replay everything everytime
+  const entries = event.Records.map((record) => {
     const { dynamodb, eventName } = record
-    if (!dynamodb) {
-      return
+    const { NewImage, OldImage } = dynamodb!
+
+    let newObject: ITodo | undefined, oldObject: ITodo | undefined
+
+    switch (eventName) {
+      case 'INSERT':
+        newObject = Converter.unmarshall(NewImage!) as ITodo
+        break
+      case 'MODIFY':
+        newObject = Converter.unmarshall(NewImage!) as ITodo
+        oldObject = Converter.unmarshall(OldImage!) as ITodo
+        break
+      case 'REMOVE':
+        oldObject = Converter.unmarshall(OldImage!) as ITodo
+        break
     }
 
-    // TODO : USE AWS.DynamoDB.Converter : https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/Converter.html
-
-    eventName === 'INSERT'
-    const { NewImage } = dynamodb
-    // const todoId = Keys!.todoId.S!
-
-    if (eventName === 'MODIFY') {
-      // update
-      // OldImage
-    } else if (eventName === 'INSERT') {
-      // create
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const todo: any = {}
-      for (const [key, value] of Object.entries(NewImage!)) {
-        // should check each S, N , ...
-        todo[key] = value.S
-      }
-      entries.push({ eventType: 'create', todo })
-    } else {
-      // delete
+    return {
+      eventName,
+      newObject,
+      oldObject,
     }
-  })
-
-  const Entries: PutEventsRequestEntryList = []
-
-  entries.forEach((entry) => {
-    Entries.push({
-      Source: 'todos-api',
-      EventBusName: 'todos',
-      DetailType: entry.eventType,
-      Detail: JSON.stringify(entry.todo),
-    })
   })
 
   const params: PutEventsRequest = {
-    Entries,
+    Entries: entries.map((entry) => {
+      const { eventName, newObject, oldObject } = entry
+      return {
+        Source: 'todos-api',
+        EventBusName: 'todo',
+        DetailType: eventName,
+        Detail: JSON.stringify({ newObject, oldObject }),
+      }
+    }),
   }
 
   try {
-    const result = await eventbridge.putEvents(params).promise()
-    console.log(result)
+    await eventbridge.putEvents(params).promise()
   } catch (err) {
     // throw a better error with tracer : err
+    // do something with them
     throw new Error('Event bridge error')
   }
 }
