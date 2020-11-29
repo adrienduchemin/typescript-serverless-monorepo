0ffa7f67c0263e75a1f9d58c57a4009d3351b7c9
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-11-29 16:46:24 +0000

    prepare fanout

--- back/stacks/todos-api/scripts/create-table.sh
+++ back/stacks/todos-api/scripts/create-table.sh
@@ -1,6 +1,7 @@
 aws dynamodb create-table \
     --table-name todos \
-    --attribute-definitions AttributeName="todoId",AttributeType=S \
-    --key-schema AttributeName="todoId",KeyType="HASH" \
+    --attribute-definitions AttributeName=todoId,AttributeType=S \
+    --key-schema AttributeName=todoId,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+    --stream-specification StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES \
     --endpoint-url http://localhost:8000 \
---
+++ back/stacks/todos-api/scripts/start-fanout.sh
@@ -0,0 +1,3 @@
+sam local invoke fanout \
+    --docker-network sam-local \
+    -e events/fanout.json
